fa4d69fd1cac6bac1128414f01c70408fbc35ffc
Akramobt <39308624+Akramobt@users.noreply.github.com>
2022-12-27 22:52:04 +0100

    connecting to Sanity and creating Herobanner

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,35 @@
 import React from 'react';
 
 import { Product,FooterBanner,HeroBanner } from '../components';
+import { client } from '../lib/client';
 
-function Home() {
+
+function Home({products,bannerData}) {
   return (
     <>
-      <HeroBanner/>
+      <HeroBanner heroBanner = { bannerData.length && bannerData[0]}/>
+      
     <div className='products-heading'>
       <h2> Best Selling Products</h2>
       <p>Speakers of many variations</p>
     
     </div>
     <div className='products-container'>
-    { ['Product1', 'Product2'].map((product)=> product)}
+    { products?.map((product)=> product.name)}
     </div>
 
     <FooterBanner/>
     </>
   )
 }
+export const getServerSideProps = async ()=> {
+  const query='*[_type=="product"]';
+  const products = await client.fetch(query);
 
+  const bannerquery='*[_type=="banner"]';
+  const bannerData = await client.fetch(bannerquery);
+  return{
+    props:{products,bannerData}
+  }
+}
 export default Home
