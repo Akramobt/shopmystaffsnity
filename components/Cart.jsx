e99c6f97019e70c18c98e3356e23844a7bca4930
Akramobt <39308624+Akramobt@users.noreply.github.com>
2023-01-07 15:00:35 +0100

    starting to creat the cart

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React , {useRef} from 'react';
+import Link from 'next/link';
+import {AiOutlineMinus, AiOutlinePlus,AiOutlineLeft, AiOutlineShpping} from 'react-icons/ai';
+import {TiDeleteOutline} from 'react-icons/ti';
+import  toast  from 'react-hot-toast';
+import { useStateContext } from '../context/StateContext';
+import {urlFor} from '../lib/client';
 
 const Cart = () => {
+  const cartRef= useRef();
+  const {totalPrice,totalQuantities,cartItems,setShowCart} = useStateContext();
   return (
-    <div>Cart</div>
+    <div className='cart-wrapper' ref={cartRef}>
+      <div className="cart-container">
+        <button 
+        type='button'
+        className='cart-heading'
+        onClick={()=> setShowCart(false)}>
+          <AiOutlineLeft/>
+          <span className='heading'>Your Cart</span>
+          <span className='cart-num-items'>({totalQuantities}items)</span>
+
+        </button>
+      </div>
+
+    </div>
   )
 }
 
