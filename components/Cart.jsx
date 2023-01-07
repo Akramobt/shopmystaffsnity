9b643d4d2a5bda056165b5e3abe2130a1198edc2
Akramobt <39308624+Akramobt@users.noreply.github.com>
2023-01-07 18:47:10 +0100

    adding cart context in case of empty shopping cart

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,6 +1,6 @@
 import React , {useRef} from 'react';
 import Link from 'next/link';
-import {AiOutlineMinus, AiOutlinePlus,AiOutlineLeft, AiOutlineShpping} from 'react-icons/ai';
+import {AiOutlineMinus, AiOutlinePlus,AiOutlineLeft, AiOutlineShopping} from 'react-icons/ai';
 import {TiDeleteOutline} from 'react-icons/ti';
 import  toast  from 'react-hot-toast';
 import { useStateContext } from '../context/StateContext';
@@ -21,6 +21,21 @@ const Cart = () => {
           <span className='cart-num-items'>({totalQuantities}items)</span>
 
         </button>
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping bag is empty</h3>
+            <Link href="/">
+              <button
+                type="button"
+                onClick={() => setShowCart(false)}
+                className="btn"
+              >
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
       </div>
 
     </div>
