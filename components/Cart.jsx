aed3182ddc705bf9aec3f4407ba5c281395895f3
Akramobt <39308624+Akramobt@users.noreply.github.com>
2023-01-26 15:30:29 +0100

    Finishing Stripe

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -5,6 +5,7 @@ import {TiDeleteOutline} from 'react-icons/ti';
 import  toast  from 'react-hot-toast';
 import { useStateContext } from '../context/StateContext';
 import {urlFor} from '../lib/client';
+import getStripe from '../lib/getStripe';
 
 const Cart = () => {
 
@@ -12,7 +13,26 @@ const Cart = () => {
     style: 'currency',
     currency: 'DZD',
   });
-  
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if(response.statusCode === 500) return;
+    
+    const data = await response.json();
+
+    toast.loading('Redirecting...');
+
+    stripe.redirectToCheckout({ sessionId: data.id });
+  }
+
   const cartRef= useRef();
   const {totalPrice,totalQuantities,cartItems,setShowCart,toggleCartItemQuanitity,onRemove} = useStateContext();
   return (
@@ -81,7 +101,7 @@ const Cart = () => {
               <h3>{DZD.format(totalPrice)}</h3>
              </div>
              <div className="btn-container">
-              <button type='button' className='btn' onClick="">
+              <button type='button' className='btn' onClick={handleCheckout}>
                 Pay with Visa
 
               </button>
